cc5df053845403708a4dc27bda04fe3c92f729f1
kurfee <abdalrhman.abdalla.moh@gmail.com>
2024-09-18 17:59:11 +0530

    the onchange function for fetching the last date and last price of the quotations for each product

--- real_estate/models/quot_fields.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-class OrderQuotationFields(models.Model):
-    _inherit = 'sale.order'
-
-    last_quotation_date = fields.Date(string="Last Quotation Date")
-    last_quotation_price = fields.Char(string="Last Quotation Price")
-
-
